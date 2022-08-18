b33be5f626edc48e60ace23ebab7b7a9d9afb153
YevaMV <eva280988@gmail.com>
2022-08-18 17:30:06 +0400

    updated

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 const storeButton = document.getElementById('store-btn');
 const retrieveButton = document.getElementById('retrieve-btn');
 
-const userId = '123';
-const user = {
-  name: 'Ann',
-  age: 22,
-};
-
 storeButton.addEventListener('click', function () {
-  sessionStorage.setItem('id', userId);
-  localStorage.setItem('user', JSON.stringify(user));
+  const userId = '123';
+  document.cookie = `id=${userId}`;
 });
 
 retrieveButton.addEventListener('click', function () {
-  const extractedId = sessionStorage.getItem('id');
-  const extractedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(extractedUser);
-
-  if (extractedId) {
-    console.log('Got the ID' + extractedId);
-  } else {
-    console.log('Could not find ID');
-  }
+  console.log(document.cookie);
 });
