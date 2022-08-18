448e3bf2edbbdb8c09f3232612a272101b2929ef
YevaMV <eva280988@gmail.com>
2022-08-18 17:52:38 +0400

    cookies

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const retrieveButton = document.getElementById('retrieve-btn');
 
 storeButton.addEventListener('click', function () {
   const userId = '123';
+  const user = { name: 'Anna', age: 22 };
   document.cookie = `id=${userId}`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrieveButton.addEventListener('click', function () {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(';');
+  const data = cookieData.map((i) => {
+    return i.trim();
+  });
+
+  console.log(data[1].split('=')[1]);
 });
